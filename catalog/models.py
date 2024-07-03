b6f516c1aeab87564b5cba2844919b350169ae6a
Marina Krasnoruzhskaya <krasnoruzhskayamarina@yandex.ru>
2024-07-03 18:23:29 +0400

    add models Category and Product

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,56 @@
 from django.db import models
+from config.settings import MEDIA_URL
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Название категории', help_text='Введите название категории', )
+    description = models.TextField(
+        verbose_name="Описание категории",
+        help_text="Введите описание категории",
+        **NULLABLE
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Название продукта', help_text='Введите название продукта', )
+    description = models.TextField(
+        verbose_name="Описание продукта",
+        help_text="Введите описание продукта",
+        **NULLABLE,
+    )
+    preview = models.ImageField(
+        upload_to=MEDIA_URL,
+        verbose_name="Изображение",
+        help_text="Загрузите изображение продукта",
+        **NULLABLE,
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        verbose_name="Категория",
+        help_text="Введите категорию продукта",
+        related_name='products',
+        **NULLABLE,
+    )
+    price = models.IntegerField(
+        verbose_name="Цена за покупку",
+        help_text="Введите цену за покупку",
+    )
+    created_at = models.DateTimeField(verbose_name="Дата создания", auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name="Дата последнего изменения", auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
