3f7a86bf0dce4699e6cd51cb0ed4e18e796ce2d2
Marina Krasnoruzhskaya <krasnoruzhskayamarina@yandex.ru>
2024-07-05 16:14:11 +0400

    update truncate_table_restart_id add select_setval_id

--- catalog/models.py
+++ catalog/models.py
@@ -19,11 +19,6 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-    @classmethod
-    def truncate_table_restart_id(cls):
-        with connection.cursor() as cursor:
-            cursor.execute(f'TRUNCATE TABLE {cls._meta.db_table} RESTART IDENTITY CASCADE')
-
     class Meta:
         verbose_name = "Категория"
         verbose_name_plural = "Категории"
