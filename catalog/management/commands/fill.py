007c44decebf73a20fef0fc35ea83ad777555173
marina <krasnoruzhskayamarina@yandex.ru>
2024-08-02 03:46:04 +0300

    update fill

--- catalog/management/commands/fill.py
+++ catalog/management/commands/fill.py
@@ -4,19 +4,22 @@ from django.core.management import BaseCommand
 from django.db import connection
 
 from blog.models import BlogPost
-from catalog.models import Product, Category, Contact
+from catalog.models import Product, Category, Contact, Version
 
 
 class Command(BaseCommand):
+    """Класс для кастомной команды заполнения БД"""
 
     @staticmethod
-    def json_read(name_file):
+    def json_read(name_file: str) -> dict:
+        """Метод считывает данные из json-файла"""
         with open(name_file, 'r', encoding='utf-8') as file:
             data = json.load(file)
         return data
 
     @staticmethod
     def json_read_data(name_app, name_model):
+        """Метод читает данные из json-файла по конкретному приложению и модели"""
         data = []
 
         for item in Command.json_read('catalog_data.json'):
@@ -27,27 +30,33 @@ class Command(BaseCommand):
 
     @staticmethod
     def truncate_table_restart_id(name_app, name_model):
+        """Метод очищает таблицу и обнуляет id=1"""
         with connection.cursor() as cursor:
             cursor.execute(f'TRUNCATE TABLE {name_app}_{name_model} RESTART IDENTITY CASCADE')
 
     @staticmethod
     def select_setval_id(name_app, name_model):
+        """Метод устанавливает последний id в таблице"""
         with connection.cursor() as cursor:
             cursor.execute(
                 f"SELECT SETVAL('{name_app}_{name_model}_id_seq', (SELECT MAX(id) FROM {name_app}_{name_model}));")
 
     def handle(self, *args, **options):
+        """Метод для заполнения БД"""
 
         Product.objects.all().delete()
         Category.objects.all().delete()
         Contact.objects.all().delete()
+        Version.objects.all().delete()
         Command.truncate_table_restart_id('catalog', 'product')
         Command.truncate_table_restart_id('catalog', 'category')
         Command.truncate_table_restart_id('catalog', 'contact')
+        Command.truncate_table_restart_id('catalog', 'version')
 
         product_for_create = []
         category_for_create = []
         contact_for_create = []
+        version_for_create = []
 
         for category in Command.json_read_data('catalog', 'category'):
             category_for_create.append(
@@ -73,11 +82,22 @@ class Command(BaseCommand):
         for contact in Command.json_read_data('catalog', 'contact'):
             contact_for_create.append(
                 Contact(id=contact["pk"], name=contact["fields"]["name"], phone=contact["fields"]["phone"],
-                         message=contact["fields"]["message"])
+                        message=contact["fields"]["message"])
             )
 
         Contact.objects.bulk_create(contact_for_create)
         Command.select_setval_id('catalog', 'contact')
+
+        for version in Command.json_read_data('catalog', 'version'):
+            version_for_create.append(
+                Version(id=version["pk"], product=Product.objects.get(pk=version["fields"]["product"]),
+                        version_number=version["fields"]["version_number"],
+                        name_version=version["fields"]["name_version"],
+                        is_current_version=version["fields"]["is_current_version"])
+            )
+
+        Version.objects.bulk_create(version_for_create)
+        Command.select_setval_id('catalog', 'version')
 
         BlogPost.objects.all().delete()
         Command.truncate_table_restart_id('blog', 'blogpost')
@@ -87,7 +107,8 @@ class Command(BaseCommand):
             blogpost_for_create.append(
                 BlogPost(id=blogpost["pk"], title=blogpost["fields"]["title"], slug=blogpost["fields"]["slug"],
                          content=blogpost["fields"]["content"], preview=blogpost["fields"]["preview"],
-                         is_published=blogpost["fields"]["is_published"], views_count=blogpost["fields"]["views_count"],
+                         is_published=blogpost["fields"]["is_published"],
+                         views_count=blogpost["fields"]["views_count"],
                          created_at=blogpost["fields"]["created_at"])
             )
 
