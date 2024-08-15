1c850c0efe094a21e09cd0ebe5dac92315e4127e
marina <krasnoruzhskayamarina@yandex.ru>
2024-08-16 00:23:18 +0300

    update fill

--- catalog/management/commands/fill.py
+++ catalog/management/commands/fill.py
@@ -1,6 +1,7 @@
 import json
 
 from django.contrib.auth.models import Group, Permission
+from django.contrib.contenttypes.models import ContentType
 from django.core.management import BaseCommand
 from django.db import connection
 
@@ -20,11 +21,11 @@ class Command(BaseCommand):
         return data
 
     @staticmethod
-    def json_read_data(name_app, name_model):
+    def json_read_data(name_file, name_app, name_model):
         """Метод читает данные из json-файла по конкретному приложению и модели"""
         data = []
 
-        for item in Command.json_read('catalog_data.json'):
+        for item in Command.json_read(name_file):
             if item["model"] == f"{name_app}.{name_model}":
                 data.append(item)
 
@@ -45,82 +46,128 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         """Метод для заполнения БД"""
+        # таблица auth.permission
 
-        Command.select_setval_id('users', 'user')
+        Permission.objects.all().delete()
+        Command.truncate_table_restart_id('auth', 'permission')
+        permission_for_create = []
 
-        Product.objects.all().delete()
-        Category.objects.all().delete()
-        Contact.objects.all().delete()
-        Version.objects.all().delete()
-        Command.truncate_table_restart_id('catalog', 'product')
-        Command.truncate_table_restart_id('catalog', 'category')
-        Command.truncate_table_restart_id('catalog', 'contact')
-        Command.truncate_table_restart_id('catalog', 'version')
-
-        product_for_create = []
-        category_for_create = []
-        contact_for_create = []
-        version_for_create = []
-
-        for category in Command.json_read_data('catalog', 'category'):
-            category_for_create.append(
-                Category(id=category["pk"], name=category["fields"]["name"],
-                         description=category["fields"]["description"])
+        for perm in Command.json_read_data('auth_data.json', 'auth', 'permission'):
+            permission_for_create.append(
+                Permission(
+                    id=perm["pk"],
+                    name=perm["fields"]["name"],
+                    content_type=ContentType.objects.get(pk=perm["fields"]["content_type"]),
+                    codename=perm["fields"]["codename"],
+                )
             )
 
-        Category.objects.bulk_create(category_for_create)
-        Command.select_setval_id('catalog', 'category')
+        Permission.objects.bulk_create(permission_for_create)
+        Command.select_setval_id('auth', 'permission')
 
-        for product in Command.json_read_data('catalog', 'product'):
-            product_for_create.append(
-                Product(id=product["pk"], name=product["fields"]["name"], description=product["fields"]["description"],
-                        preview=product["fields"]["preview"],
-                        category=Category.objects.get(pk=product["fields"]["category"]),
-                        price=product["fields"]["price"], created_at=product["fields"]["created_at"],
-                        updated_at=product["fields"]["updated_at"],
-                        user=User.objects.get(pk=product["fields"]["user"]),
-                        is_published=product["fields"]["is_published"]
-                        )
+        # таблица auth.group
+
+        Group.objects.all().delete()
+        Command.truncate_table_restart_id('auth', 'group')
+        group_for_create = []
+        group_permissions = {}
+
+        for group in Command.json_read_data('auth_data.json', 'auth', 'group'):
+            permissions = []
+            for perm_id in group["fields"]["permissions"]:
+                permissions.append(Permission.objects.get(id=perm_id))
+            group_permissions[group["pk"]] = permissions
+
+            group_for_create.append(
+                Group(
+                    id=group["pk"],
+                    name=group["fields"]["name"],
+                )
             )
 
-        Product.objects.bulk_create(product_for_create)
-        Command.select_setval_id('catalog', 'product')
+        Group.objects.bulk_create(group_for_create)
+        Command.select_setval_id('auth', 'group')
 
-        for contact in Command.json_read_data('catalog', 'contact'):
-            contact_for_create.append(
-                Contact(id=contact["pk"], name=contact["fields"]["name"], phone=contact["fields"]["phone"],
-                        message=contact["fields"]["message"])
-            )
+        for pk, permissions in group_permissions.items():
+            group = Group.objects.get(pk=pk)
+            group.permissions.set(permissions)
 
-        Contact.objects.bulk_create(contact_for_create)
-        Command.select_setval_id('catalog', 'contact')
 
-        for version in Command.json_read_data('catalog', 'version'):
-            version_for_create.append(
-                Version(id=version["pk"], product=Product.objects.get(pk=version["fields"]["product"]),
-                        version_number=version["fields"]["version_number"],
-                        name_version=version["fields"]["name_version"],
-                        is_current_version=version["fields"]["is_current_version"])
-            )
-
-        Version.objects.bulk_create(version_for_create)
-        Command.select_setval_id('catalog', 'version')
-
-        BlogPost.objects.all().delete()
-        Command.truncate_table_restart_id('blog', 'blogpost')
-
-        blogpost_for_create = []
-        for blogpost in Command.json_read('blog_data.json'):
-            blogpost_for_create.append(
-                BlogPost(id=blogpost["pk"], title=blogpost["fields"]["title"], slug=blogpost["fields"]["slug"],
-                         content=blogpost["fields"]["content"], preview=blogpost["fields"]["preview"],
-                         is_published=blogpost["fields"]["is_published"],
-                         views_count=blogpost["fields"]["views_count"],
-                         created_at=blogpost["fields"]["created_at"],
-                         author=User.objects.get(pk=blogpost["fields"]["author"]))
-            )
-
-        BlogPost.objects.bulk_create(blogpost_for_create)
-        Command.select_setval_id('blog', 'blogpost')
+        # Command.select_setval_id('users', 'user')
+        #
+        # Product.objects.all().delete()
+        # Category.objects.all().delete()
+        # Contact.objects.all().delete()
+        # Version.objects.all().delete()
+        # Command.truncate_table_restart_id('catalog', 'product')
+        # Command.truncate_table_restart_id('catalog', 'category')
+        # Command.truncate_table_restart_id('catalog', 'contact')
+        # Command.truncate_table_restart_id('catalog', 'version')
+        #
+        # product_for_create = []
+        # category_for_create = []
+        # contact_for_create = []
+        # version_for_create = []
+        #
+        # for category in Command.json_read_data('catalog_data.json', 'catalog', 'category'):
+        #     category_for_create.append(
+        #         Category(id=category["pk"], name=category["fields"]["name"],
+        #                  description=category["fields"]["description"])
+        #     )
+        #
+        # Category.objects.bulk_create(category_for_create)
+        # Command.select_setval_id('catalog', 'category')
+        #
+        # for product in Command.json_read_data('catalog_data.json', 'catalog', 'product'):
+        #     product_for_create.append(
+        #         Product(id=product["pk"], name=product["fields"]["name"], description=product["fields"]["description"],
+        #                 preview=product["fields"]["preview"],
+        #                 category=Category.objects.get(pk=product["fields"]["category"]),
+        #                 price=product["fields"]["price"], created_at=product["fields"]["created_at"],
+        #                 updated_at=product["fields"]["updated_at"],
+        #                 user=User.objects.get(pk=product["fields"]["user"]),
+        #                 is_published=product["fields"]["is_published"]
+        #                 )
+        #     )
+        #
+        # Product.objects.bulk_create(product_for_create)
+        # Command.select_setval_id('catalog', 'product')
+        #
+        # for contact in Command.json_read_data('catalog_data.json', 'catalog', 'contact'):
+        #     contact_for_create.append(
+        #         Contact(id=contact["pk"], name=contact["fields"]["name"], phone=contact["fields"]["phone"],
+        #                 message=contact["fields"]["message"])
+        #     )
+        #
+        # Contact.objects.bulk_create(contact_for_create)
+        # Command.select_setval_id('catalog', 'contact')
+        #
+        # for version in Command.json_read_data('catalog_data.json', 'catalog', 'version'):
+        #     version_for_create.append(
+        #         Version(id=version["pk"], product=Product.objects.get(pk=version["fields"]["product"]),
+        #                 version_number=version["fields"]["version_number"],
+        #                 name_version=version["fields"]["name_version"],
+        #                 is_current_version=version["fields"]["is_current_version"])
+        #     )
+        #
+        # Version.objects.bulk_create(version_for_create)
+        # Command.select_setval_id('catalog', 'version')
+        #
+        # BlogPost.objects.all().delete()
+        # Command.truncate_table_restart_id('blog', 'blogpost')
+        #
+        # blogpost_for_create = []
+        # for blogpost in Command.json_read('blog_data.json'):
+        #     blogpost_for_create.append(
+        #         BlogPost(id=blogpost["pk"], title=blogpost["fields"]["title"], slug=blogpost["fields"]["slug"],
+        #                  content=blogpost["fields"]["content"], preview=blogpost["fields"]["preview"],
+        #                  is_published=blogpost["fields"]["is_published"],
+        #                  views_count=blogpost["fields"]["views_count"],
+        #                  created_at=blogpost["fields"]["created_at"],
+        #                  author=User.objects.get(pk=blogpost["fields"]["author"]))
+        #     )
+        #
+        # BlogPost.objects.bulk_create(blogpost_for_create)
+        # Command.select_setval_id('blog', 'blogpost')
 
 
