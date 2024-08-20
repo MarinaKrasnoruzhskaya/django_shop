2a80564e2f9736e242b8bfa5f9c344b5e2d9d09b
marina <krasnoruzhskayamarina@yandex.ru>
2024-08-20 13:55:20 +0300

    add redis

--- config/settings.py
+++ config/settings.py
@@ -155,3 +155,13 @@ AUTH_USER_MODEL = 'users.User'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+CACHE_ENABLED = os.getenv('CACHE_ENABLED') == 'True'
+
+if CACHE_ENABLED:
+    CACHES = {
+        "default": {
+            "BACKEND": "django.core.cache.backends.redis.RedisCache",
+            "LOCATION": os.getenv('CACHES_LOCATION'),
+        }
+    }
